50bcf75ee87d79aac719d1de3eb2e4facccb71bd
Axy <alexmauster@mail.ru>
2019-12-22 00:50:42 +0700

    Add sorting.

--- app/food-menu/table/food-menu-table.component.js
+++ app/food-menu/table/food-menu-table.component.js
@@ -7,4 +7,18 @@ angular.module('foodMenu').component('foodMenuTable', {
 
 function foodMenuTableCtrl($scope, foodMenuDataService) {
     $scope.dataService = foodMenuDataService;
+    $scope.order = '';
+
+    $scope.setOrderBy = function (order) {
+        switch ($scope.order) {
+            case '-' + order:
+                $scope.order = '+' + order;
+                break;
+            case '+' + order:
+                $scope.order = '';
+                break;
+            default:
+                $scope.order = '-' + order;
+        }
+    }
 }
