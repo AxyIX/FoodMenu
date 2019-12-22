a001e20ff00f0e01be6ab11299caa52a3b586c0f
Axy <alexmauster@mail.ru>
2019-12-22 17:06:31 +0700

    Add modal ingredients view.

---
+++ app/food-menu/ingredients-dialog/ingredients-dialog.component.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('foodMenu').component('ingredientsDialog', {
+    templateUrl: 'food-menu/ingredients-dialog/ingredients-dialog.template.html',
+    bindings: {
+        resolve: '<',
+        close: '&'
+    },
+    controller: ingredientsDialogCtrl
+});
+
+function ingredientsDialogCtrl() {
+    const ctrl = this;
+    ctrl.$onInit = () => {
+        ctrl.title = ctrl.resolve.title;
+        ctrl.ingredients = ctrl.resolve.ingredients;
+    };
+    ctrl.ok = function () {
+        ctrl.close();
+    };
+}
--- app/food-menu/table/food-menu-table.component.js
+++ app/food-menu/table/food-menu-table.component.js
@@ -1,24 +1,42 @@
 'use strict';
 
 angular.module('foodMenu').component('foodMenuTable', {
-        templateUrl: 'food-menu/table/food-menu-table.template.html',
-        controller: foodMenuTableCtrl
-    });
+    templateUrl: 'food-menu/table/food-menu-table.template.html',
+    controller: foodMenuTableCtrl
+});
 
-function foodMenuTableCtrl($scope, foodMenuDataService) {
+function foodMenuTableCtrl($scope, $uibModal, foodMenuDataService) {
     $scope.dataService = foodMenuDataService;
     $scope.order = '';
 
     $scope.setOrderBy = function (order) {
         switch ($scope.order) {
-            case '-' + order:
-                $scope.order = '+' + order;
-                break;
             case '+' + order:
+                $scope.order = '-' + order;
+                break;
+            case '-' + order:
                 $scope.order = '';
                 break;
             default:
-                $scope.order = '-' + order;
+                $scope.order = '+' + order;
         }
+    }
+
+    this.openComponentModal = function (title, ingredients) {
+        $uibModal.open({
+            animation: true,
+            component: 'ingredientsDialog',
+            resolve: {
+                title: function () {
+                    return title;
+                },
+                ingredients: function () {
+                    return ingredients;
+                }
+            },
+            close: function () {
+                $uibModal.close();
+            }
+        }).result.then(function () {}, function () {});
     }
 }
